f3a6cf8e2b4b67a47cbacfd4653e81990940db93
MicheleFumi <michele.fumi96@gmail.com>
2024-11-04 16:04:16 +0100

    bug fix

--- public/js/main.js
+++ public/js/main.js
@@ -8,11 +8,13 @@ axios.get("http://127.0.0.1:3000/post")
   
     listElement= ''
     posts.forEach(post => {
-        let {title, content, img, tags} = post
+        console.log(post);
+        
+        let {title, content, image, tags} = post
         const markup=`
             <li class="pb-2 text-center"><h2>${title}</h2></li>
             <li class="pb-2 text-center">${content}</li>
-            <li class="pb-2 text-center"><img src="${img}" alt=""></li>
+            <li class="pb-2 text-center"><img src="${image}" alt=""></li>
             <li class="pb-2 text-center">${tags}</li>
             
             `
